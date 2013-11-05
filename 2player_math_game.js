7a4e9855ead6b46306404822e9348c09ec4c2f4b
James Targett <someguynamedjamestargett@gmail.com>
2013-11-05 04:28:24 +0000

    last commit --bugs fixed --complete

--- 2player_math_game.js
+++ 2player_math_game.js
@@ -3,6 +3,15 @@ var player2Lives = 3;
 
 var currentPlayer = 1;
 
+
+function changePlayer() {
+	if (currentPlayer == 1) {
+		currentPlayer = 2;
+	} else {
+		currentPlayer = 1;
+	}
+}
+
 function promptUser(operator){
 	if (operator === 1) {
 		operator = "added to";
@@ -14,24 +23,16 @@ function promptUser(operator){
 	return prompt("Player " + currentPlayer + ": " + "What is " + x + " " + operator + " " + y + "?");
 }
 
-function changePlayer() {
-	if (currentPlayer == 1)
-		currentPlayer = 2;
-	else
-		currentPlayer = 1;
-}
-
 while ((player1Lives > 0) && (player2Lives > 0)) {
 	if (currentPlayer == 1) {
 		console.log("Player 1. Your turn.");
 	} else {
 		console.log("Player 2. Your turn.");
 	}
-	
+
 	var x = Math.floor(Math.random() * 20);
 	var y = Math.floor(Math.random() * 20);
 	var op = Math.floor(Math.random() * 3);
-	
 	var answer = promptUser(op);
 
 	if (op == 1) {
@@ -42,12 +43,10 @@ while ((player1Lives > 0) && (player2Lives > 0)) {
 		correctAnswer = (x * y);
 	}
 
-	console.log("answer: " + answer + " correctAnswer: " + correctAnswer);
-
-	if (answer === correctAnswer.toString()) {
+	if (answer == correctAnswer) {
 		console.log("Right answer. Bet you think you're really smart, huh?")
 	} else {
-		if (currentPlayer === 1) {
+		if (currentPlayer == 1) {
 			player1Lives = player1Lives - 1;
 			console.log("WRONG!!!!.... (haha). Lose a life. " + player1Lives + " remaining.");
 		} else {
@@ -55,6 +54,5 @@ while ((player1Lives > 0) && (player2Lives > 0)) {
 			console.log("WRONG!!!!.... (haha). Lose a life. " + player2Lives + " remaining.");
 		}		
 	}
-
-	break;
-};
+	changePlayer();
+}
