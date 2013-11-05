ffbb08804c4b157e53e9ab39e641e137eda0f1d9
James Targett <someguynamedjamestargett@gmail.com>
2013-11-05 03:34:57 +0000

    third commit --close to completion

--- 2player_math_game.js
+++ 2player_math_game.js
@@ -3,8 +3,26 @@ var player2Lives = 3;
 
 var currentPlayer = 1;
 
+function promptUser(operator){
+	if (operator === 1) {
+		operator = "added to";
+	} else if (operator === 2) {
+		operator = "subtracted by";
+	} else {
+		operator = "multiplied by";
+	}
+	return prompt("Player " + currentPlayer + ": " + "What is " + x + " " + operator + " " + y + "?");
+}
+
+function changePlayer() {
+	if (currentPlayer == 1)
+		currentPlayer = 2;
+	else
+		currentPlayer = 1;
+}
+
 while ((player1Lives > 0) && (player2Lives > 0)) {
-	if (currentPlayer === 1) {
+	if (currentPlayer == 1) {
 		console.log("Player 1. Your turn.");
 	} else {
 		console.log("Player 2. Your turn.");
@@ -13,33 +31,30 @@ while ((player1Lives > 0) && (player2Lives > 0)) {
 	var x = Math.floor(Math.random() * 20);
 	var y = Math.floor(Math.random() * 20);
 	var op = Math.floor(Math.random() * 3);
-	if (op === 1) {
-		op = "added to";
-	} else if (op === 2) {
-		op = "subtracted by";
+	
+	var answer = promptUser(op);
+
+	if (op == 1) {
+		correctAnswer = (x + y);
+	} else if (op == 2) {
+		correctAnswer = (x - y);
 	} else {
-		op = "multiplied by";
+		correctAnswer = (x * y);
 	}
 
-	answer = prompt("Player " + currentPlayer + ": " + "What is " + x + " " + op + " " + y + "?");
-	if (op === 1) {
-		correctAnswer = x + y;
-	} else if (op === 2) {
-		correctAnswer = x - y;
-	} else {
-		correctAnswer = x * y;
-	}
+	console.log("answer: " + answer + " correctAnswer: " + correctAnswer);
 
-	if (answer != correctAnswer) {
+	if (answer === correctAnswer.toString()) {
+		console.log("Right answer. Bet you think you're really smart, huh?")
+	} else {
 		if (currentPlayer === 1) {
 			player1Lives = player1Lives - 1;
-			console.log("Lose a life. You have " + player1Lives + " remaining.");
+			console.log("WRONG!!!!.... (haha). Lose a life. " + player1Lives + " remaining.");
 		} else {
 			player2Lives = player2Lives - 1;
-			console.log("Lose a life. You have " + player2Lives + " remaining.");
-		}
+			console.log("WRONG!!!!.... (haha). Lose a life. " + player2Lives + " remaining.");
+		}		
 	}
-
 
 	break;
 };
